aa903ef2086ee9ac9b16335823d9bbf0a92f6e44
BetoCruz <humbertocruz.sp@gmail.com>
2023-10-03 16:59:08 -0300

    Subindo o Screenmatch

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(//@JsonAlias({"Title","Titulo"}) String titulo,
+                         @JsonAlias({"Title","Titulo"}) String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao
+                         //@JsonProperty("imdbVotes") String votos
+                         ) {
+}
